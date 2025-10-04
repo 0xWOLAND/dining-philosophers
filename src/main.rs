de1af128c9a77d54436d8ed94e606ae2867127ad
Bhargav Annem <bannem@caltech.edu>
2025-10-04 13:26:24 +0100

    fix: async

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio::time::{sleep, Duration};
 
-fn philosophers(n: usize) {
+async fn philosophers(n: usize) {
     let forks: Vec<_> = (0..n).map(|_| Arc::new(Mutex::new(()))).collect();
     let mut handles = vec![];
 
@@ -10,21 +10,24 @@ fn philosophers(n: usize) {
         let left = Arc::clone(&forks[i]);
         let right = Arc::clone(&forks[(i + 1) % n]);
         
-        handles.push(thread::spawn(move || loop {
-            thread::sleep(Duration::from_millis(1));
-            let _left = left.lock().unwrap();
-            let _right = right.lock().unwrap();
-            thread::sleep(Duration::from_millis(1));
+        handles.push(tokio::spawn(async move {
+            loop {
+                sleep(Duration::from_millis(1)).await;
+                let _left = left.lock().await;
+                let _right = right.lock().await;
+                sleep(Duration::from_millis(1)).await;
+            }
         }));
     }
 
     for h in handles {
-        h.join().unwrap();
+        h.await.unwrap();
     }
 }
 
-fn main() {
-    philosophers(5);
+#[tokio::main]
+async fn main() {
+    philosophers(5).await;
 }
 
 #[cfg(test)]
@@ -35,8 +38,9 @@ mod tests {
     proptest! {
         #[test]
         fn test_deadlock(n in 2usize..8) {
-            let handle = thread::spawn(move || philosophers(n));
-            thread::sleep(Duration::from_secs(1));
+            let rt = tokio::runtime::Runtime::new().unwrap();
+            let handle = rt.spawn(async move { philosophers(n).await });
+            std::thread::sleep(std::time::Duration::from_secs(1));
             prop_assert!(!handle.is_finished(), "Should deadlock!");
         }
     }
